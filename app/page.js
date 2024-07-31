4d07a7a64601dc8e708ab12eb872f93df908127a
uttkarshTewari2006 <149989607+uttkarshTewari2006@users.noreply.github.com>
2024-07-31 00:44:22 -0700

    tried to fix deployment errors

--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
 "use client";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   addDoc,
   collection,
-  getDoc,
-  QuerySnapshot,
-  query,
   onSnapshot,
   deleteDoc,
   doc,
+  query,
 } from "firebase/firestore";
 import { db } from "./firebase";
 
@@ -19,21 +16,19 @@ function capitalize(str) {
 }
 
 export default function Home() {
-  let zero = 0;
-
   useEffect(() => {
     const q = query(collection(db, "items"));
-    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let itemsArr = [];
       let totalCurrent = 0;
-      QuerySnapshot.forEach((doc) => {
+      querySnapshot.forEach((doc) => {
         itemsArr.push({ ...doc.data(), id: doc.id });
         totalCurrent += Number(doc.data().quantity) * Number(doc.data().price);
       });
       setItems(itemsArr);
       setTotal(totalCurrent);
-      return () => unsubscribe;
     });
+    return () => unsubscribe();
   }, []);
 
   const [items, setItems] = useState([
@@ -41,9 +36,11 @@ export default function Home() {
     { name: "Movie", price: 24.95, quantity: 1 },
     { name: "coffee", price: 4.95, quantity: 1 },
   ]);
-  let isInArray;
-  let quant;
-  //add item to database
+
+  const [total, setTotal] = useState(34.85);
+  const [newItem, setNewItem] = useState({ name: "", price: "", quantity: "" });
+  const [search, setSearch] = useState("");
+
   const addItem = async (e) => {
     e.preventDefault();
     const existingItemIndex = items.findIndex(
@@ -61,8 +58,10 @@ export default function Home() {
         price: Number(newItem.price),
         quantity: Number(newItem.quantity),
       };
-      setItems([...items, newItemData]);
-      setTotal(total + newItemData.price * newItemData.quantity);
+      setItems((prevItems) => [...prevItems, newItemData]);
+      setTotal(
+        (prevTotal) => prevTotal + newItemData.price * newItemData.quantity
+      );
 
       await addDoc(collection(db, "items"), newItemData);
       setNewItem({ name: "", price: "", quantity: "" });
@@ -73,7 +72,9 @@ export default function Home() {
             ...item,
             quantity: Number(item.quantity) + Number(newItem.quantity),
           };
-          setTotal(total + item.price * Number(newItem.quantity));
+          setTotal(
+            (prevTotal) => prevTotal + item.price * Number(newItem.quantity)
+          );
           return updatedItem;
         }
         return item;
@@ -83,23 +84,20 @@ export default function Home() {
     }
   };
 
-  //read items from database
-
-  //delete item from database
-  const [total, setTotal] = useState(34.85);
-  const [newItem, setNewItem] = useState({ name: "", price: "", quantity: "" });
-
-  const [search, setSearch] = useState("");
-
   const deleteItem = async (id) => {
     await deleteDoc(doc(db, "items", id));
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
+
   return (
     <main className="bg-black-500 flex min-h-screen flex-col items-center justify-between sm:p-24 p-4 w-full">
       <div className="z-10 max-w-3xl w-full items-center justify-between font-mono text-sm">
         <h1 className="block text-4xl p-4 text-center w-100">Pantry Tracker</h1>
-        <div className="p-2 bg-indigo-800 big-slate-800  sm:pl-4 pl-24 sm:pr-4 pr-24 rounded-lg w-full ">
-          <form className="p-2 w-full flex flex-col items-center ">
+        <div className="p-2 bg-indigo-800 big-slate-800 sm:pl-4 pl-24 sm:pr-4 pr-24 rounded-lg w-full ">
+          <form
+            className="p-2 w-full flex flex-col items-center "
+            onSubmit={addItem}
+          >
             <div className="w-full flex flex-row justify-center">
               <input
                 value={newItem.name}
@@ -108,8 +106,8 @@ export default function Home() {
                 }
                 type="text"
                 placeholder="enter item"
-                className="bg-black borde text-white sm:mr-2 mr-6 p-3 sm:p-1  rounded-lg"
-              ></input>
+                className="bg-black borde text-white sm:mr-2 mr-6 p-3 sm:p-1 rounded-lg"
+              />
               <input
                 value={newItem.price}
                 onChange={(e) =>
@@ -117,8 +115,8 @@ export default function Home() {
                 }
                 type="number"
                 placeholder="enter $"
-                className="bg-black text-white sm:mr-2 pr-6 sm:ml-2 ml-6 p-3 sm:p-1 sm:w-24 text-black rounded-lg"
-              ></input>
+                className="bg-black text-white sm:mr-2 pr-6 sm:ml-2 ml-6 p-3 sm:p-1 sm:w-24 rounded-lg"
+              />
               <input
                 value={newItem.quantity}
                 onChange={(e) =>
@@ -126,10 +124,9 @@ export default function Home() {
                 }
                 type="number"
                 placeholder="enter amount"
-                className="bg-black text-white sm:mr-2 pr-6 sm:ml-2 ml-6 p-3 sm:p-1 sm:w-32 text-black rounded-lg"
-              ></input>
+                className="bg-black text-white sm:mr-2 pr-6 sm:ml-2 ml-6 p-3 sm:p-1 sm:w-32 rounded-lg"
+              />
               <button
-                onClick={addItem}
                 type="submit"
                 className="bg-black text-white sm:mr-2 pr-6 sm:ml-2 ml-6 p-3 sm:p-1 rounded-lg sm:pl-2 pl-5 sm:pr-2 pr-5 hover:opacity-80"
               >
@@ -137,13 +134,11 @@ export default function Home() {
               </button>
             </div>
             <input
-              onChange={(e) => {
-                setSearch(e.target.value);
-              }}
+              onChange={(e) => setSearch(e.target.value)}
               type="text"
               placeholder="search"
-              className="m-2 bg-black text-white sm:mr-2 pr-6 sm:ml-2 ml-6 p-3 sm:p-1 sm:w-32 text-black rounded-lg"
-            ></input>
+              className="m-2 bg-black text-white sm:mr-2 pr-6 sm:ml-2 ml-6 p-3 sm:p-1 sm:w-32 rounded-lg"
+            />
           </form>
 
           <ul>
@@ -155,17 +150,17 @@ export default function Home() {
               </div>
             </li>
             {items
-              .filter((Item) => {
-                return Item.name.indexOf(search) != -1;
-              })
+              .filter((item) => item.name.includes(search))
               .map((item, index) => (
-                <li>
-                  <div key={item.name} className="flex justify-between mt-2">
+                <li key={item.id}>
+                  <div className="flex justify-between mt-2">
                     <div className="w-16 lg:w-24 sm:w-28">
                       <button
-                        className=" bg-white text-red-500 pl-1 pr-1 ml-1 mr-1 hover:opacity-80 rounded-lg"
+                        className="bg-white text-red-500 pl-1 pr-1 ml-1 mr-1 hover:opacity-80 rounded-lg"
                         onClick={() => {
-                          setTotal(total + Number(item.price));
+                          setTotal(
+                            (prevTotal) => prevTotal + Number(item.price)
+                          );
                           const newItems = items.map((itm, indx) => {
                             if (indx === index) {
                               return {
@@ -181,12 +176,16 @@ export default function Home() {
                         +
                       </button>
                       <button
-                        className=" bg-white text-red-500 pl-1 pr-1 mr-1 hover:opacity-80 rounded-lg"
+                        className="bg-white text-red-500 pl-1 pr-1 mr-1 hover:opacity-80 rounded-lg"
                         onClick={() => {
-                          setTotal(total - Number(item.price));
+                          setTotal(
+                            (prevTotal) => prevTotal - Number(item.price)
+                          );
                           if (item.quantity === 1) {
-                            setItems(
-                              items.filter((entry) => entry.name !== item.name)
+                            setItems((prevItems) =>
+                              prevItems.filter(
+                                (entry) => entry.name !== item.name
+                              )
                             );
                           } else {
                             const newItems = items.map((itm, indx) => {
@@ -217,15 +216,14 @@ export default function Home() {
                         {Number(item.price).toFixed(2)}
                       </span>
                       <button
-                        className=" bg-white text-red-500 pl-1 pr-1 hover:opacity-80 rounded-lg"
+                        className="bg-white text-red-500 pl-1 pr-1 hover:opacity-80 rounded-lg"
                         onClick={() => {
                           setTotal(
-                            total - Number(item.price) * Number(item.quantity)
+                            (prevTotal) =>
+                              prevTotal -
+                              Number(item.price) * Number(item.quantity)
                           );
                           deleteItem(item.id);
-                          setItems(
-                            items.filter((entry) => entry.name !== item.name)
-                          );
                         }}
                       >
                         X
@@ -236,7 +234,7 @@ export default function Home() {
               ))}
           </ul>
 
-          {items.length == 0 || (
+          {items.length > 0 && (
             <div className="flex flex-row justify-between">
               <strong className="pt-2 text-2xl">total</strong>
               <span className="text-lg pt-2">${total.toFixed(2)}</span>
