b3e124893ad70f351a05d88c1fe3eb54947833de
uttkarsh <uttkarsh.tewari1234@gmail.com>
2024-07-31 01:10:35 -0700

    fixed deployment issue

--- app/firebase.js
+++ app/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import { useEffect } from "react";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +20,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+useEffect(() => {
+  const analytics = getAnalytics(app);
+}, []);
 
 export const db = getFirestore(app);
