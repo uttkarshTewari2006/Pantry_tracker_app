30c273a614847d00ee4323140c1d447e60742da9
uttkarsh <uttkarsh.tewari1234@gmail.com>
2024-08-01 14:05:04 -0700

    hidden api keys

--- app/firebase.js
+++ app/firebase.js
@@ -2,26 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { useEffect } from "react";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDRoDk_11CfOo5328UHKRoUOMhrFldQSnw",
-  authDomain: "expense-tracker-a9d2e.firebaseapp.com",
-  projectId: "expense-tracker-a9d2e",
-  storageBucket: "expense-tracker-a9d2e.appspot.com",
-  messagingSenderId: "520187757242",
-  appId: "1:520187757242:web:04b88c0f48f200a4e2b883",
-  measurementId: "G-563QDX9ZPN",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-useEffect(() => {
-  const analytics = getAnalytics(app);
-}, []);
 
 export const db = getFirestore(app);
